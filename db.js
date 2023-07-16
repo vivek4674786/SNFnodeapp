029d9ee9309d21bf6be8c18e286eb8534087dbb2
Vivek Kandoliya <90150720+vivek4674786@users.noreply.github.com>
2023-07-16 14:29:33 +0530

    Update db.js

--- db.js
+++ db.js
@@ -8,8 +8,6 @@ require("dotenv").config();
 const mongoose = require("mongoose")
 
 const mongoURI = process.env.MONGO_URI;
-//"mongodb+srv://smartandfastrajkot:weare3members@smart-and-fast-rajkot.w6dvfzg.mongodb.net/?retryWrites=true&w=majority"
-//const mongoURI = "mongodb://localhost:27017/s&fRajkot"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI)
